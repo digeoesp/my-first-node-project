3f2b5f854616288b0d56f566887c1155750eaec6
digeoesp <diegocrypto@outlook.com>
2021-02-01 10:51:02 -0800

    add other modules

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const myModule = require('./myModule')
 console.log('hello world')
-
+const myOtherModule  = require('./myOtherModule')
+const anotherModuleFile = require('./anotherModuleFile')
 function hello(name){
     console.log(`hello, ${name}`);
 }
 hello('diego')
-
 hello('mari')
+
+console.log(myModule.add(2,2));
+console.log(myModule.subtract(5,4))
+myOtherModule.welcomeToNode()
+anotherModuleFile.hello()
